f2327cbe059d5e701ce9379d145ad98ecc426bfe
viditd1-eng <vidit.d1@ahduni.edu.in>
2026-08-19 18:31:48 +0530

    Implement subset question_77 of leetcode using backtracking

---
+++ 77_subset.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> current;
+
+    void backtrack(vector<int>& nums,int start){
+        ans.push_back(current);
+        for(int i=start;i<nums.size();i++){
+            if(i>start && nums[i] == nums[i-1]){
+                continue;
+            }
+            current.push_back(nums[i]);
+
+            backtrack(nums,i+1);
+            current.pop_back();
+        }
+    }
+    vector<vector<int>> subsets(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        backtrack(nums,0);
+        return ans;
+    }
+};
